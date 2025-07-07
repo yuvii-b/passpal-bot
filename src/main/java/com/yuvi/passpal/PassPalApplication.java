de2c437d26a159e127aee7ed5ae326ac7c3cbd45
yuvaraj <yuvaraj.codes@gmail.com>
2025-07-07 09:04:18 +0530

    initial setup

---
+++ src/main/java/com/yuvi/passpal/PassPalApplication.java
@@ -0,0 +1,13 @@
+package com.yuvi.passpal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PassPalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PassPalApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/yuvi/passpal/PassPalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yuvi.passpal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PassPalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
